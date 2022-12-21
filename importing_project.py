ebb7528320df1a4204ceb0411c3f3710ee10513b
vsomani0 <vsomani0@gmail.com>
2022-12-20 19:26:44 -0800

    BasicBot

--- importing_project.py
+++ importing_project.py
@@ -1,7 +1,21 @@
-import matplotlib as mp
-import numpy as np
-import pandas as pd
+import discord
 
-import csv
-print(3)
-data = pd.read_csv(r'C:\Users\vsoma_ogh6dsk\Downloads\FanGraphs Leaderboard.csv')
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+    
+@client.event
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startsWith("$hello"):
+        await message.channel.send("Hello!")
+
+client.run("MTA1NDk1ODQxMTg5ODQyMTI1OA.Gwerg-.S2l7Kvg4niT0t0MNBuYIjnz8fuIrBnHBV8LwSg")
