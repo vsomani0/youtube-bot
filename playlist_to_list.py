93817712602985614b2b315e34f87e9327243ff3
vsomani0 <vsomani0@gmail.com>
2023-01-04 09:53:45 -0800

    Made function rand_vid_category insert to a list instead of deleting to make it faster and easier to modify. Also fixed minor naming errors of simple lists being called playlists

--- playlist_to_list.py
+++ playlist_to_list.py
@@ -2,7 +2,7 @@ from pytube import YouTube, Playlist
 import pytube.exceptions
 import random
 
-def get_playlist(playlist: Playlist):
+def get_playlist(playlist: Playlist) -> list:
     '''Stores all chosen video data from a playlist into a list.'''
     all_vids = []
     print(f'Downloading all video data from playlist \"{playlist.title}\". Please wait a few moments.')
@@ -26,48 +26,43 @@ def get_video_details(curr_vid: YouTube) -> list:
     # Add time of video in minutes to list
     curr_vid_details.append(curr_vid.views)
     curr_vid_details.append(curr_vid.author)
+    curr_vid_details.append(False)
+    # By default, all videos are false, to indicate they are not a favorite
     return curr_vid_details
 
-def add_vid_to_playlist(playlist: Playlist, curr_vid: YouTube) -> None:
-    '''Adds video curr_vid to playlist'''
-    playlist.append(get_video_details(curr_vid))
-
-def rand_vid(playlist: Playlist) -> list:
+def rand_vid(all_vids) -> list:
     '''Returns a random video's settings from a playlist'''
-    if (len(playlist) <= 0):
+    if (len(all_vids) <= 0):
         print("No video found!")
         return []
-    random_video_index = random.randint(0, len(playlist)-1)
-    return playlist[random_video_index]
+    random_video_index = random.randint(0, len(all_vids)-1)
+    return all_vids[random_video_index]
 
-def rand_vid_category(playlist, min_length = -1, max_length = -1, min_views = -1, max_views = -1, author = ""):
+def rand_vid_category(all_vids, min_length = -1, max_length = -1, min_views = -1, max_views = -1, author = ""):
     '''Takes a playlist and some chosen categories, and gives a video from that category. Deletes all videos from
-    different categories in a temporary list, and then takes a random video out of the temporary playlist.'''
-    new_playlist = playlist
-    if (min_length != -1):
-        for i in reversed(range(0, len(new_playlist))):
-            if (new_playlist[i][2] < min_length):
-                del(new_playlist[i])
-    if (max_length != -1):
-        for i in reversed(range(0, len(new_playlist))):
-            if (new_playlist[i][2] > max_length):
-                del(new_playlist[i])
-    if (min_views != -1):
-        for i in reversed(range(0, len(new_playlist))):
-            if (new_playlist[i][3] < min_views):
-                del(new_playlist[i][3])
-    if (max_views != -1):
-        for i in reversed(range(0, len(new_playlist))):
-            if (new_playlist[i][3] > max_views):
-                del(new_playlist[i])
-    if (author != ""):
-        for i in reversed(range(0, len(new_playlist))):
-            if (new_playlist[i][4].casefold() != author.casefold()):
-                del(new_playlist[i])
-    if (len(playlist) <= 0):
+    different categories in a temporary list, and then takes a random video out of the temporary list.'''
+    temp_list = []
+    for vid_details in all_vids:
+        if (min_length != -1):
+            if (vid_details[2] < min_length):
+                continue
+        if (max_length != -1):
+            if (vid_details[2] > max_length):
+                continue
+        if (min_views != -1):
+            if (vid_details[3] < min_views):
+                continue
+        if (max_views != -1):
+            if (vid_details[3] > max_views):
+                continue
+        if (author != ""):
+            if (vid_details[4].casefold() != author.casefold()):
+                continue
+        temp_list.append(vid_details)
+    if (len(temp_list) <= 0):
         print("No video found with the search criteria!")
         return []
-    return(rand_vid(new_playlist))
+    return(rand_vid(temp_list))
 
 all_playlists = []
 pl_link = "https://www.youtube.com/playlist?list=PLEhSYc84M4xCljuyXNxEgVHLgdCzAm0vu"
@@ -76,8 +71,8 @@ playlist_reference = {}
 playlist_reference[playlist_one.title] = len(all_playlists)
 # Dictionary matches up playlist name with index in list, allowing user to call a playlist by its name.
 all_playlists.append(get_playlist(playlist_one))
-for x in range(5):
-    print(rand_vid_category(all_playlists[playlist_reference["Favorite Videos"]], author = 'secret base'))
+for x in range(10):
+    print(rand_vid_category(all_playlists[playlist_reference["Favorite Videos"]], min_length = 30, max_length = 500, min_views = 20))
 
 
 
